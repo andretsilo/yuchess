b7c5e398cb37129cc40cba3782dc45cca71423e5
zeroParanoie <andrea.tsilogiannis@gmail.com>
2025-08-08 15:17:26 +0200

    Add endpoint

---
+++ matchmaking/src/main/java/com/yuchess/matchmaking/server/controller/MatchmakingController.java
@@ -0,0 +1,17 @@
+package com.yuchess.matchmaking.server.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/matchmaking")
+public class MatchmakingController {
+
+	@PostMapping("/join")
+	public ResponseEntity<Void> join() {
+		return ResponseEntity.ok().build();
+	}
+
+}
